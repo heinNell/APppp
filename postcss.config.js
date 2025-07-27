5c1753277351028c69a87de9b43881a7c9c509b1
heinNell <heinnell64@gmail.com>
2025-07-27 09:17:22 +0200

    Updated postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
 export default {
   plugins: {
-    'postcss-nesting': {},
     tailwindcss: {},
     autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
-  }
-};
+  },
+}
